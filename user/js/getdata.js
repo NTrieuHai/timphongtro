33de8280d69f0ec2d0612d72b923ca5cf4d8e4c3
NTrieuHai <113275416+NTrieuHai@users.noreply.github.com>
2024-03-06 13:43:08 +0700

    add user js

---
+++ user/js/getdata.js
@@ -0,0 +1,81 @@
+// Chuyên mục
+$(document).ready(function () {
+    $.ajax({
+      url: "http://localhost:8088/timphongtro/include/get-categories-data.php",
+      dataType: "json",
+      success: function (data) {
+        $("#categories").html("");
+        for (i = 0; i < data.length; i++) {
+          var categories = data[i];
+          var str = `<option value="${categories["id"]}">
+          ${categories["classify"]}</option>`;
+          $("#categories").append(str);
+        }
+      },
+    });
+  });
+  
+  // Lấy tỉnh
+  $(document).ready(function () {
+    $.ajax({
+      url: "http://localhost:8088/timphongtro/include/get-city-data.php",
+      dataType: "json",
+      success: function (data) {
+        $("#city").html("");
+        for (i = 0; i < data.length; i++) {
+          var city = data[i];
+          var str = `<option value="${city["id"]}">${city["name"]}</option>`;
+          $("#city").append(str);
+        }
+  
+        $("#city").on("change", function (e) {
+          getDistrict();
+        });
+      },
+    });
+  });
+  
+  // Lấy huyện
+  
+  function getDistrict() {
+    var idCity = $("#city").val();
+    $.ajax({
+      url:
+        "http://localhost:8088/timphongtro/include/get-district-data.php?city_id=" +
+        idCity,
+      dataType: "json",
+      success: function (data) {
+        $("#district").html("");
+        for (i = 0; i < data.length; i++) {
+          var district = data[i];
+          var str = ` 
+            <option value="${district["id"]}">${district["name"]}</option>`;
+          $("#district").append(str);
+        }
+        $("#district").on("change", function (e) {
+          getWard();
+        });
+      },
+    });
+  }
+  
+  // Lấy xã
+  function getWard() {
+    var idDistrict = $("#district").val();
+    $.ajax({
+      url:
+        "http://localhost:8088/timphongtro/include/get-ward-data.php?district_id=" +
+        idDistrict,
+      dataType: "json",
+      success: function (data) {
+        $("#ward").html("");
+        for (i = 0; i < data.length; i++) {
+          var ward = data[i];
+          var str = ` 
+            <option value="${ward["id"]}">${ward["name"]}</option>`;
+          $("#ward").append(str);
+        }
+      },
+    });
+  }
+  
